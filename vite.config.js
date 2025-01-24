37abaa8c09793f8598a9114194c35d05c55345fb
AllanYen <iamallan0917@gmail.com>
2025-01-24 17:23:17 +0800

    20250124 1723

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base:process.env.NODE_env === 'production' ? '/sixHector-react-week5/' : ''
+  base:process.env.NODE_env === 'production' ? '/sixHector-react-homework/' : ''
 });
